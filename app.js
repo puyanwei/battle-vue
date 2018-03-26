ddb6f8eca19f4002076f876e55e959cad56bcef1
Puyan Wei <puyanwei@gmail.com>
2018-03-26 15:24:54 +0100

    created #attack, #specialAttack, #heal, #restart, and #randomNumber

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ new Vue({
     methods: {
         startGame: function() {
             this.isRunning = true;
+            this.playerHealth = 100;
+            this.computerHealth = 100;
+        },
+        attack: function() {
+            this.computerHealth -= this.randomNumber(10, 3);
+        },
+        specialAttack: function() {
+            this.computerHealth -= this.randomNumber(30, 5);
+        },
+        heal: function() {
+            this.playerHealth += 10;
+        },
+        restart: function() {
+            this.isRunning = false;
+            this.playerHealth = 100;
+            this.computerHealth = 100;
+        },
+        randomNumber: function(min, max) {
+            return Math.floor(Math.random() * min, max);
         },
     },
 });
